22f166ca8c63e777c08b1b52c2671de931d2af98
celvine adi putra <celvinadiputra2@gmail.com>
2024-03-15 21:30:12 +0700

    Update : [Requirements.txt]

---
+++ real_or_fake_job_transform.py
@@ -0,0 +1,19 @@
+
+import tensorflow as tf
+
+LABEL_KEY = "fraudulent"
+FEATURE_KEY = "full_description"
+
+
+def transformed_name(key):
+    return key + "_xf"
+
+
+def preprocessing_fn(inputs):
+    outputs = {}
+
+    outputs[transformed_name(FEATURE_KEY)] = tf.strings.lower(inputs[FEATURE_KEY])
+
+    outputs[transformed_name(LABEL_KEY)] = tf.cast(inputs[LABEL_KEY], tf.int64)
+
+    return outputs
--- utils.py
+++ utils.py
@@ -10,7 +10,6 @@ nltk.download('stopwords')
 # python -m spacy download en_core_web_sm
 nlp = spacy.load('en_core_web_sm')
 
-
 def clean_text(text: str) -> str:
     text = text.lower()
 
